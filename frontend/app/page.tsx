2d4933cbbf49b66068f5985af84a80d8fcfcc860
Asout3 <mikiyimer98@gmail.com>
2025-04-08 12:18:46 +0000

    commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h2>hello bitches i am here </h2>
+      <Link href="/dash"><button>dash</button></Link>
     </div>
   );
 }
