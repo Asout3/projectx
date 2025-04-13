18515f1db79d7bea99d641a95cfcb74fdfeb4def
Asout3 <mikiyimer98@gmail.com>
2025-04-13 09:38:10 +0000

    nah

--- frontend/components/SignUpForm.tsx
+++ frontend/components/SignUpForm.tsx
@@ -6,14 +6,17 @@ import { getRedirectResult } from 'firebase/auth';
 
 const SignIn = () => {
   useEffect(() => {
+    console.log('starting...')
     const fetchRedirectResult = async () => {
       // Adding a small delay to make sure the redirect result is processed
       setTimeout(async () => {
         try {
           const response = await getRedirectResult(auth);
           console.log(response);
+          console.log('it seems like it might work')
         } catch (error) {
           console.log('Error retrieving redirect result: ', error);
+          console.log('bitch');
         }
       }, 200);  // Adjust delay if needed (100ms to 300ms)
     };
